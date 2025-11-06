d8f987d26d5e480e18740a376d9f16a3f9ff6724
user1 <user1@DESKTOP-HH37C8G>
2025-11-06 15:02:42 +0900

    [INIT] ShapeUp 웹 프로젝트

---
+++ ShapeUpBoot/src/main/java/com/ShapeUp/boot/ShapeUpBootApplication.java
@@ -0,0 +1,13 @@
+package com.ShapeUp.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShapeUpBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShapeUpBootApplication.class, args);
+	}
+
+}
