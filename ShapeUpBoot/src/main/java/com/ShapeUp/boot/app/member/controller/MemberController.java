d8aee56b8a386ac1635288d6821064c35ed9d08b
user1 <user1@DESKTOP-HH37C8G>
2025-11-11 17:03:24 +0900

    [ADD]회원가입 메인페이지 구현 회원가입 메인페이지 구현 완료 MemberController 클래스 생성 내용 구현은 아직

---
+++ ShapeUpBoot/src/main/java/com/ShapeUp/boot/app/member/controller/MemberController.java
@@ -0,0 +1,12 @@
+package com.ShapeUp.boot.app.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+	
+}
