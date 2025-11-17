06529a7bab0d598f03cd2cbe6236a4810e8c00bb
user1 <user1@DESKTOP-HH37C8G>
2025-11-17 14:14:19 +0900

    [ADD] Controller 누락 Controller가 누락되어 다시 업로드함

--- ShapeUpBoot/src/main/java/com/ShapeUp/boot/app/user/controller/UserController.java
+++ ShapeUpBoot/src/main/java/com/ShapeUp/boot/app/user/controller/UserController.java
@@ -2,11 +2,27 @@ package com.ShapeUp.boot.app.user.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 public class UserController {
-
+	@GetMapping("/user/signupAgreement")
+	public String signupAgreement() {
+	    return "user/signupAgreement"; // /WEB-INF/views/user/signupAgreement.jsp
+	}
+	@GetMapping("/user/signupInsertInfo")
+	public String signupsignupInsertInfo() {
+	    return "user/signupsignupInsertInfo"; // /WEB-INF/views/user/signupAgreement.jsp
+	}
+	@GetMapping("/user/signupSuccess")
+	public String signupSuccess() {
+	    return "user/signupSuccess"; // /WEB-INF/views/user/signupAgreement.jsp
+	}
+	@GetMapping("/user/signupSurvey")
+	public String signupSurvey() {
+	    return "user/signupSurvey"; // /WEB-INF/views/user/signupAgreement.jsp
+	}
 }
