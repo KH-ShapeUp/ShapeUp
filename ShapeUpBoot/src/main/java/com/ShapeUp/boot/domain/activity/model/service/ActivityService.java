486aa5879e171d4d11b200a847dcb13537c3b5c3
user1 <user1@DESKTOP-1N3EIEO>
2025-11-28 11:37:18 +0900

    [ADD] 커밋 추가

--- ShapeUpBoot/src/main/java/com/ShapeUp/boot/domain/activity/model/service/ActivityService.java
+++ ShapeUpBoot/src/main/java/com/ShapeUp/boot/domain/activity/model/service/ActivityService.java
@@ -2,15 +2,13 @@ package com.ShapeUp.boot.domain.activity.model.service;
 
 import java.util.List;
 
-import com.ShapeUp.boot.app.activity.controller.dto.ActivityInsertDto;
-
 import com.ShapeUp.boot.domain.activity.model.vo.ActivityLogVO;
 import com.ShapeUp.boot.domain.activity.model.vo.ActivityVO;
 
 public interface ActivityService {
 
-	List<ActivityVO> getActivityListByKeyword(String keyword);
+   List<ActivityVO> getActivityListByKeyword(String keyword);
 
-	int insertActivities(ActivityLogVO log);
+   int insertActivities(ActivityLogVO log);
 
 }
